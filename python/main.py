3ea704449b5a22debc47c4d54c3c75736bf3e832
Todd DeLuca <nospam@todddeluca.com>
2013-06-11 20:31:03 -0400

    Concatenate minotar files into a single file.

--- python/main.py
+++ python/main.py
@@ -2023,12 +2023,33 @@ def gen_microcosm_targets(species, version='v5'):
 # MinoTar
 
 
+def minotar_dir():
+    # version is made up b/c I can not find a version on the minotar site.
+    return os.path.join(config.datadir, 'minotar', 'v1')
+
+
+def minotar_fly_dir():
+    return os.path.join(minotar_dir(), 'drosophila_melanogaster')
+
+
+def minotar_human_dir():
+    return os.path.join(minotar_dir(), 'homo_sapiens')
+
+
+def minotar_fly_csv_path():
+    return os.path.join(minotar_fly_dir(), 'DrosConservedSeedsConservedTargs.csv')
+
+
+def minotar_human_csv_path():
+    return os.path.join(minotar_human_dir(), 'HumanConservedSeedsConservedTargs.csv')
+
+
 def download_minotar():
 
-    # version is made up b/c I can not find a version on the minotar site.
-    minotar_dir = os.path.join(config.datadir, 'minotar', 'v1')
-    human_dir = os.path.join(minotar_dir, 'homo_sapiens')
-    fly_dir = os.path.join(minotar_dir, 'drosophila_melanogaster')
+    print 'Downloadding MinoTar supporting data from TargetScanVert version vert_50.'
+    vert50_dir = os.path.join(targetscan_top_dir(), 'vert_50')
+    url = 'http://www.targetscan.org//vert_50/vert_50_data_download/miR_Family_Info.txt.zip'
+    download_and_unzip(url, vert50_dir)
 
     print 'Downloading MinoTar supporting data from TargetScanFly version fly_52orfs.'
     orfs_dir = os.path.join(targetscan_top_dir(), 'fly_52orfs')
@@ -2041,13 +2062,61 @@ def download_minotar():
 
     print 'Downloading MinoTar data files.'
     url = 'http://www.flyrnai.org/supplement/HumanUniqueMirnas.txt'
-    download_and_unzip(url, human_dir)
+    download_and_unzip(url, minotar_human_dir())
     url = 'http://www.flyrnai.org/supplement/HumanConservedSeedsConservedTargs.zip'
-    download_and_unzip(url, human_dir)
+    download_and_unzip(url, minotar_human_dir())
     url = 'http://www.flyrnai.org/supplement/DrosUniqueMirnas.txt'
-    download_and_unzip(url, fly_dir)
+    download_and_unzip(url, minotar_fly_dir())
     url = 'http://www.flyrnai.org/supplement/DrosConservedSeedsConservedTargs.zip'
-    download_and_unzip(url, fly_dir)
+    download_and_unzip(url, minotar_fly_dir())
+
+
+def concatenate_minotar_fly():
+    '''
+    Concatenate the dozens of files downloaded from minotar, each of which
+    encodes the targetscan mir family in the filename, into a single csv file
+    where the (unencoded) mir family is prepended as the first field of each
+    line.
+    '''
+    targetfiles_dir = os.path.join(minotar_fly_dir(), 'ConservedSeedsConservedTargs')
+    def family_parser(fn):
+        return os.path.splitext(os.path.basename(fn).replace('_', '/'))[0]
+
+    concatenate_minotar_sub(targetfiles_dir, family_parser, minotar_fly_csv_path())
+
+
+def concatenate_minotar_human():
+    '''
+    Concatenate the dozens of files downloaded from minotar, each of which
+    encodes the targetscan mir family in the filename, into a single csv file
+    where the (unencoded) mir family is prepended as the first field of each
+    line.
+    '''
+    targetfiles_dir = os.path.join(minotar_human_dir(), 'ConservedSeedsConservedTargs')
+    def family_parser(fn):
+        return os.path.splitext(os.path.basename(fn).replace(':', '/'))[0]
+
+    concatenate_minotar_sub(targetfiles_dir, family_parser, minotar_human_csv_path())
+
+
+def concatenate_minotar_sub(files_dir, family_parser, outfile):
+    with open(outfile, 'w') as fh:
+        for fn in os.listdir(files_dir):
+            if not fn.endswith('.txt'):
+                print 'Warning: file not ending in ".txt" dir={}, file={}'.format(files_dir, fn)
+                continue
+            mir_family = family_parser(fn)
+            print mir_family
+            with open(os.path.join(files_dir, fn)) as infh:
+                for line in infh:
+                    if not line.strip():
+                        raise Exception('Unexpected blank line', fn, line)
+                    if line.strip().startswith('#'):
+                        raise Exception('Unexpected comment line', fn, line)
+                    fh.write('{},{}'.format(mir_family, line))
+
+
+
 
 
 
